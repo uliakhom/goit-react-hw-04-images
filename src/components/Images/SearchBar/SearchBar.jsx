d6b753ce03425e9b8a8df0476b80ff6999500a33
uliakhom <ulianakhomenko@gmail.com>
2022-05-29 04:06:53 +0300

    Update SearchBar.jsx

--- src/components/Images/SearchBar/SearchBar.jsx
+++ src/components/Images/SearchBar/SearchBar.jsx
@@ -37,55 +37,6 @@ const SearchBar = ({ onSubmit }) => {
   );
 };
 
-// class SearchBar extends Component {
-//   state = {
-//     q: '',
-//   };
-
-//   handleChange = ({ target }) => {
-//     const { name, value } = target;
-//     this.setState({
-//       [name]: value,
-//     });
-//   };
-
-//   handleSubmit = e => {
-//     e.preventDefault();
-//     this.props.onSubmit({ ...this.state });
-//     this.reset();
-//   };
-
-//   reset() {
-//     this.setState({
-//       q: '',
-//     });
-//   }
-
-//   render() {
-//     const { handleChange, handleSubmit } = this;
-//     const { q } = this.state;
-//     return (
-//       <header className={styles.searchbar}>
-//         <form onSubmit={handleSubmit} className={styles.form}>
-//           <button type="submit" className={styles.button}>
-//             <span className={styles.label}>Search</span>
-//           </button>
-
-//           <input
-//             className={styles.input}
-//             value={q}
-//             onChange={handleChange}
-//             type="text"
-//             name="q"
-//             placeholder="Search images and photos"
-//             required
-//           />
-//         </form>
-//       </header>
-//     );
-//   }
-// }
-
 export default SearchBar;
 
 SearchBar.propTypes = {
